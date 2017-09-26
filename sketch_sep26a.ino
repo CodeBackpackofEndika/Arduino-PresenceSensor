bd291277701a14a2b97aaa3b1f46403240426e7d
CodeBackpackofEndika <32270483+CodeBackpackofEndika@users.noreply.github.com>
2017-09-26 13:22:51 +0200

    Add files via upload

---
+++ sketch_sep26a.ino
@@ -0,0 +1,19 @@
+Byte Led=11; 
+byte Ldr=0; 
+int valor; 
+ 
+ 
+void setup() { 
+   
+pinMode(Led,OUTPUT); 
+digitalWrite(Led,LOW); 
+Serial.begin(9600); 
+} 
+ 
+void loop() { 
+ 
+valor=analogRead(Ldr); 
+valor=valor/4; 
+analogWrite(Led,valor); 
+Serial.println(valor); 
+} 
